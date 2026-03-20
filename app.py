0b941c5e81c7adff809dcc5418162f83c76b3347
9·58 <jmg@958.fr>
2026-03-20 16:22:35 +0100

    v3.13 bump version string

--- app.py
+++ app.py
@@ -720,7 +720,7 @@ def generate_pdf(data):
 
 @app.route("/")
 def health():
-    return jsonify({"service": "Barbier PDF Generator", "status": "ok", "version": "3.12"})
+    return jsonify({"service": "Barbier PDF Generator", "status": "ok", "version": "3.13"})
 
 
 @app.route("/generate-pdf-by-ref", methods=["GET", "POST"])
